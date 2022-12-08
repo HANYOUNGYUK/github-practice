d943d08d561bed8ad5241a4375732732fc814c44
mac <mac@macui-MacBookPro.local>
2022-12-08 15:38:46 +0900

    2022-12-08

---
+++ Java/src/계산기/test04.java
@@ -0,0 +1,24 @@
+package 계산기;
+
+import java.util.Scanner;
+
+public class test04 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int age;
+		String name;
+		
+		System.out.println("---입력---");
+		System.out.println("이름 입력");
+		name = input.next();
+
+		System.out.print("나이: ");
+		age = input.nextInt();
+		System.out.println("--출력---");
+		System.out.println("이름: " + name);
+		System.out.println("나이: " + age);
+		
+	}
+
+}
